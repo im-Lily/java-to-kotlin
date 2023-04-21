0f8a6d7893092138568da108bd3f2425b07cddc2
im-Lily <dmsruddlvhs2013@gmail.com>
2023-04-21 09:38:20 +0900

    [Feat]: 8강 내용 추가

---
+++ src/main/kotlin/lec08/Lec08Main.java
@@ -0,0 +1,13 @@
+package lec08;
+
+public class Lec08Main {
+    public static void repeat(String str, int num, boolean useNewLine) {
+        for (int i = 1; i <= num; i++) {
+            if (useNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.println(str);
+            }
+        }
+    }
+}
--- src/main/kotlin/lec08/Lec08Main.kt
+++ src/main/kotlin/lec08/Lec08Main.kt
@@ -6,6 +6,21 @@ fun main() {
      * named argument : 매개변수 이름을 통해 직접 지정 / 지정되지 않은 매개변수는 기본값 사용
      */
     repeat("World", useNewLine = false)
+
+    printNameAndGender(name = "lily", gender = "FEMALE")
+
+    /**
+     * Java 의 함수를 사용할 때는 named argument 를 사용할 수 없음
+    Lec08Main.repeat(str = "Hello")
+     */
+
+    printAll("A", "B", "C") // 바로 값 대입 가능
+
+    /**
+     * 가변인자 함수 사용 시 배열을 바로 넣는 대신 스프레드 연산자(*) 사용
+     */
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
 }
 /**
  * 함수가 하나의 결과값이라면, block 부분을 = 으로 쓸 수 있음
@@ -22,7 +37,7 @@ fun max(a: Int, b: Int): Int =
 
 /**
  * = 를 사용하는 경우 반환 타입 생략 가능
- * block {} 을 사용하는 경우, 반환 타입이 Unit이 아니면 명시적으로 작성 필요
+ * block {} 을 사용하는 경우, 반환 타입이 Unit 이 아니면 명시적으로 작성 필요
  */
 fun max(a: Int, b: Int) = if (a > b) a else b
 
@@ -39,3 +54,17 @@ fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true) {
     }
 }
 
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+/**
+ * 같은 타입의 여러 파라미터 받기 (가변인자)
+ * vararg
+ */
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
